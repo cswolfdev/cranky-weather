2cbac5e1ea69bc15d943393cf1fb333053b80361
Christopher Wolf <chris@chriswolf.net>
2024-08-09 16:23:24 -0400

    some initial markup and styling

---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,30 @@
+
+const WeatherApp = () => {
+  return (
+    <div className="container">
+        <div className="weather-app">
+            <div className="search">
+                <div className="search-top">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <div className="location">London</div>
+                </div>
+                <div className="search-bar">
+                    <input type="text" placeholder="Enter Location" />
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+            <div className="weather">
+                <div className="current-conditions">Clear</div>
+                <div className="temperature">80&deg;</div>
+            </div>
+            <div className="weather-date">
+                <p>Friday, August 9</p>
+            </div>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default WeatherApp
